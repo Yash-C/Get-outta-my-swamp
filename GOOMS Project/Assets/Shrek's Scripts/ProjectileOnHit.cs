b81282fa7e793e65a94af65bd0ef5315509cc42c
Jeeerus <40648688+Jeeerus@users.noreply.github.com>
2018-10-21 17:39:21 +1300

    New Scripts
    Added new scripts for enemey damage and prefab disappearing on hit

---
+++ GOOMS Project/Assets/Shrek's Scripts/ProjectileOnHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileOnHit : MonoBehaviour {
+
+    // USE THIS SCRIPT ON OUR MUD PREFAB (The mud object sphere thing in our assets
+    // folder. This basically removes the object if it collides with something that has OnTrigger
+    // ticked (Which will only be our enemies.
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
